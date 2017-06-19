159c8d2f8d49ae1ecfe2c3e3726f4c7fd54ce226
vinhomn <vinhomn@gmail.com>
2017-06-19 16:36:00 +0700

    rename folders in MessageBus project

---
+++ src/CHV.Infrastructure.MessageBus/RequestResponsePattern/IMessageRespondent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CHV.Infrastructure.MessageBus
+{
+    public interface IMessageRespondent : IDisposable
+    {
+    }
+}
